8a00c1d7addb4a6e07a2ac986ea97a6f0ee04797
Matheus Catarino <matheus-catarino@hotmail.com>
2022-11-14 15:31:41 -0300

    init upgrade

--- build.zig
+++ build.zig
@@ -1,18 +1,31 @@
 const std = @import("std");
-const deps = @import("deps.zig");
+const builtin = @import("builtin");
 
 const Builder = std.build.Builder;
 
 pub fn build(b: *Builder) void {
-    var target = b.standardTargetOptions(.{});
-    if (target.isGnuLibC()) target.abi = .musl;
+    std.debug.print("\nZig Info:\n\nVersion: {}\nStage: {s}\n", .{ builtin.zig_version, @tagName(builtin.zig_backend) });
 
+    b.prominent_compile_errors = true;
+    const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
+    const lmdb = b.addStaticLibrary("lmdb", null);
+    lmdb.setTarget(target);
+    lmdb.setBuildMode(mode);
+    // zig fmt: off
+    lmdb.addCSourceFiles(
+        &[_][]const u8{ "vendor/lmdb/libraries/liblmdb/mdb.c", "vendor/lmdb/libraries/liblmdb/midl.c" },
+        &[_][]const u8{ "-Oz", "-fno-sanitize-trap", "-Wall", "-Wno-unused-parameter", "-Wbad-function-cast", "-Wuninitialized" });
+    lmdb.linkLibC();
+    lmdb.install();
+    // zig fmt: on
     const tests = b.addTest("lmdb.zig");
     tests.setTarget(target);
+    tests.use_stage1 = true;
     tests.setBuildMode(mode);
-    deps.addAllTo(tests);
+    tests.addIncludePath("vendor/lmdb/libraries/liblmdb");
+    tests.linkLibrary(lmdb);
 
     const test_step = b.step("test", "Run libary tests");
     test_step.dependOn(&tests.step);
